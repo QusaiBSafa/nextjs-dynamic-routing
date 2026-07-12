bf9df86d523c7b0b636545ce4a42181525250a79
Qusai Safa <153466377+QusaiBSafa@users.noreply.github.com>
2026-07-12 03:02:24 +0300

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import userRoutes from './routes/api';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
